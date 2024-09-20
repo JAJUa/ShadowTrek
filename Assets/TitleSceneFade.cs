70821757389073095b19cc74442a3d8f0abb4b5e
PrettyDuna <127952072+JAJUa@users.noreply.github.com>
2024-09-20 19:28:07 +0900

    UI수정, 버그 수정, 타이틀 추가

---
+++ Assets/TitleSceneFade.cs
@@ -0,0 +1,62 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleSceneFade : MonoBehaviour
+{
+    [SerializeField] private Button startBtn;
+    [SerializeField] private Button settingBtn;
+    [SerializeField] private Button quitBtn;
+
+    [SerializeField] Animator anim;
+
+    float targetAlpha;
+
+    private void Start()
+    {
+        targetAlpha = 5f / 255f;
+
+        startBtn.onClick.AddListener(() => StartBtn());
+        settingBtn.onClick.AddListener(() => SettingBtn());
+        quitBtn.onClick.AddListener(() => QuitBtn());
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            anim.SetTrigger("Skip");
+        }
+    }
+
+    public void StartBtn()
+    {
+        startBtn.GetComponent<Image>().DOFade(targetAlpha, 1f).OnComplete(() =>
+        {
+            FadeInFadeOut.Inst.NextScene();
+        });
+    }
+
+    public void SettingBtn()
+    {
+        settingBtn.GetComponent<Image>().DOFade(targetAlpha, 1f);
+    }
+
+    public void QuitBtn()
+    {
+        quitBtn.GetComponent<Image>().DOFade(targetAlpha, 1f).OnComplete(() =>
+        {
+            FadeInFadeOut.Inst.FadeIn();
+
+            DOVirtual.DelayedCall(1f, () => {
+                #if UNITY_EDITOR
+                                UnityEditor.EditorApplication.isPlaying = false;
+                #else
+                                    Application.Quit(); // 어플리케이션 종료
+                #endif
+            });
+        });
+    }
+}
