48e5b9dbfbff4749dcd48994044912ec3798bf5f
Oriatbf <john31534249@gmail.com>
2024-09-21 18:43:20 +0900

    책 영상 수정 3까지

--- Assets/02.Scripts/Dialogue/LampRotDialogue.cs
+++ Assets/02.Scripts/Dialogue/LampRotDialogue.cs
@@ -7,6 +7,7 @@ public class LampRotDialogue : Dialouge
     [SerializeField] TurnLight m_lamp;
     public override void Interact()
     {
+        Debug.Log("램프 클릭");
         m_lamp.TurnReverse();
         base.Interact();
     }
